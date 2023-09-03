f8cfdedd9e5b1c1b80b0c8b6e0255f4e4dcfae78
Cyrus Xu <czxu@pal-nat186-10-231.itap.purdue.edu>
2023-09-03 19:37:48 -0400

    setup basics

---
+++ run.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env ./node_modules/.bin/ts-node-script
+
+function printsmthn(s: string) {
+  console.log("Your string: " + s);
+  return null;
+}
+
+printsmthn("Hello World");
+console.log("hi there");
