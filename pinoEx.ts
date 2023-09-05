a547fb43775e8d108e322c2658d8ffbafbd76166
David <davidasousa335@gmail.com>
2023-09-04 23:54:25 -0400

    9/4/2023 11:55 Config For Pino

--- pinoEx.ts
+++ pinoEx.ts
@@ -1,3 +1,7 @@
 import logger from './src/logger.ts';
 
 logger.info('Hello');
+logger.error('Hello');
+logger.fatal('Hello');
+
+// Just Run ts-node pinoEx.ts as usual and all of the logging data should show up
--- src/logger.ts
+++ src/logger.ts
@@ -1,9 +1,17 @@
+//https://www.youtube.com/watch?v=2kKeQl_m8iY&ab_channel=RichardOliverBray
+
 import pino from 'pino';
 
 const logger = pino({
     transport: {
-        target: "pino-pretty"
+        target: "pino-pretty",
+        options: {
+            translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
+            ignore: "pid,hostname",
+        }
     }
 });
+
+
 
 export default logger;
