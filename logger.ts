5130ac1dd267e68821b385fb7f8c4f908de8d34d
David <davidasousa335@gmail.com>
2023-09-05 14:54:27 -0400

    Submitting Pino

--- logger.ts
+++ logger.ts
@@ -1,6 +1,7 @@
 //https://www.youtube.com/watch?v=2kKeQl_m8iY&ab_channel=RichardOliverBray
 
 import pino from 'pino';
+// const fs = require("fs");
 
 const logger = pino({
     transport: {
@@ -9,8 +10,10 @@ const logger = pino({
             translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
             ignore: "pid,hostname",
         },
-    }
-});
+    },
+},
+// pino.destination('./logs.log')
+);
 
 export function logMsg(msg: string, option?: string) {
     if(option == "info" || option === undefined) {
