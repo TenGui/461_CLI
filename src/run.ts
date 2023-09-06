1434915ad6cbeb401cc70a2205553ddc02b69a2a
TenGui <84549255+TenGui@users.noreply.github.com>
2023-09-06 14:07:53 -0400

    Added logic to run, handled errors (#32)

--- src/run.ts
+++ src/run.ts
@@ -1,20 +1,21 @@
 #!/usr/bin/env node
-import * as run_install from "./install/runInstall";
+import * as runInstall from "./install/installProcess";
 
 if (process.argv.length != 3) {
-  console.log("Usage: run.ts <filename>");
+  console.log("Usage: \n./run install\n./run <url_file>\n./run test");
   process.exit(1);
 }
 
 if (process.argv[2] == "install") {
-  const installResult: boolean = run_install.install(
-    "/package.json",
-    "/logs",
-    "install.log"
-  );
-  if (installResult) {
-    console.log("Installation completed successfully.");
-  } else {
-    console.log("Installation failed.");
-  }
+  const installResult: boolean = runInstall.install();
+  installResult ? process.exit(0) : process.exit(1);
+} else if (process.argv[2] == "test") {
+  console.log("Test not yet implemented");
+  process.exit(1);
+} else {
+  const filePath: string = process.argv[2];
+  //Check if argv[2] is a valid relative path
+  process.exit(1);
 }
+
+process.exit(-1);
