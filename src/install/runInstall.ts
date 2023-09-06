14a05de6e4fc257983d0f087aa78a64835d12345
Cyrus Xu <czxu@pal-nat186-185-225.itap.purdue.edu>
2023-09-05 21:23:02 -0400

    Adjusted install code and compilation

--- src/install/runInstall.ts
+++ src/install/runInstall.ts
@@ -1,30 +1,50 @@
 #!/usr/bin/env ./node_modules/.bin/ts-node-script
 import * as child_process from "child_process";
 import * as fs from "fs";
+import * as path from "path";
 
+function install(
+  packagePath: string,
+  logFolderPath: string,
+  fileName: string
+): boolean {
+  const fullPackagePath = path.join(process.cwd(), packagePath);
+  const dirPath = path.join(process.cwd(), logFolderPath);
+  const filePath = path.join(dirPath, fileName);
+  const dependencies = getDependencies(fullPackagePath);
 
-function install(packagePath: string, logFilePath: string): boolean {
-    const dependencies = getDependencies(packagePath);
-    for (let dependency in dependencies) {
-        try{
-          const command = `npm install ${dependency}`;
-          const result = child_process.execSync(command, {stdio: 'pipe', encoding: 'utf-8'});
-          fs.appendFileSync(logFilePath, `=== ${dependency} Installation ===\n`);
-          fs.appendFileSync(logFilePath, `STDOUT:\n${result}\n`);
-          console.log(`Installation of ${dependency} completed successfully.`);
-        } catch (error) {
-            console.log(`Installation of ${dependency} failed.`);
-            console.log(error);
-            return false
-        }
+  //Check if directories already exist
+  if (!fs.existsSync(dirPath)) {
+    try {
+      fs.mkdirSync(dirPath, { recursive: true });
+    } catch (err) {
+      console.error(`Error creating folder '${dirPath}': ${err}`);
     }
-    return true;
+  }
+
+  //Install Dependencies
+  for (let dependency in dependencies) {
+    try {
+      const command = `npm install ${dependency}`;
+      const result = child_process.execSync(command, {
+        stdio: "pipe",
+        encoding: "utf-8",
+      });
+      fs.appendFileSync(filePath, `=== ${dependency} Installation ===\n`);
+      fs.appendFileSync(filePath, `STDOUT:\n${result}\n`);
+      console.log(`Installation of ${dependency} completed successfully.`);
+    } catch (error) {
+      console.log(`Installation of ${dependency} failed.`);
+      console.log(error);
+      return false;
+    }
+  }
+  return true;
 }
 
-
-function getDependencies(path: string){
-  let packageJson = JSON.parse(fs.readFileSync(path).toString());
+function getDependencies(path: string) {
+  const packageJson = JSON.parse(fs.readFileSync(path).toString());
   return packageJson.devDependencies;
 }
 
-export {install};
+export { install };
--- src/run.ts
+++ src/run.ts
@@ -1,28 +1,20 @@
-//title
-
+#!/usr/bin/env node
 import * as run_install from "./install/runInstall";
-function printsmthn(s: string) {
-  console.log("Your string: " + s);
-  return null;
-}
-
-printsmthn("Hello World");
-console.log("hi there");
-
 
 if (process.argv.length != 3) {
   console.log("Usage: run.ts <filename>");
   process.exit(1);
 }
 
-
 if (process.argv[2] == "install") {
-
-  let installResult:boolean = run_install.install(process.cwd() + '/package.json', process.cwd() + '/log.txt');
+  const installResult: boolean = run_install.install(
+    "/package.json",
+    "/logs",
+    "install.log"
+  );
   if (installResult) {
     console.log("Installation completed successfully.");
-  }
-  else {
+  } else {
     console.log("Installation failed.");
   }
 }
