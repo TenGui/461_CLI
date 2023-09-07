ae76cce6b2b7af8f68cc6fe0b9681d9a0706c472
CadenBPurdue <cadenbrennan@gmail.com>
2023-09-07 13:17:56 -0400

    urlReader addition

---
+++ src/url_list/urlReader.ts
@@ -0,0 +1,30 @@
+class URLReader {
+    private url: string;
+
+    constructor(url: string) {
+        this.url = url;
+    }
+
+    isNpmURL(): boolean {
+        const npmRegex = /^https:\/\/(www\.)?npmjs\.com\/package\/[\w-]+$/;
+        return npmRegex.test(this.url);
+    }
+
+    isGitHubURL(): boolean {
+        const githubRegex = /^https:\/\/github\.com\/[\w-]+\/[\w-]+$/;
+        return githubRegex.test(this.url);
+    }
+
+    extractPackageName(): string | null {
+        if (this.isNpmURL()) {
+            // Extract package name from npmjs.com URL
+            const matches = this.url.match(/\/package\/([\w-]+)$/);
+            return matches ? matches[1] : null;
+        } else if (this.isGitHubURL()) {
+            // Extract repository name from GitHub URL
+            const matches = this.url.match(/\/([\w-]+)\/([\w-]+)$/);
+            return matches ? `${matches[1]}/${matches[2]}` : null;
+        }
+        return null;
+    }
+ }
