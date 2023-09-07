8ed6c0bc746479b3a8c4af99f044790a80f1c7f1
SmenNeoris <140518960+smneoris@users.noreply.github.com>
2023-09-07 12:10:08 -0400

    Update jest.config.js for Jest setup

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    testMatch: ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"],
+    testEnvironment: "node",
+    transform: {
+      "^.+\\.js$": "babel-jest",
+    },
+    setupFiles: ["<rootDir>/setupTests.js"], // Run setupTests.js before tests.
+    collectCoverage: true, // Enable code coverage reporting.
+    coverageReporters: ["html", "text-summary"], // Specify coverage report formats.
+    reporters: ["default"], // Use the default test reporter.
+    moduleNameMapper: {
+      // Map module imports to mock files.
+      "\\.(css|less|scss)$": "identity-obj-proxy",
+    },
+  };
+  
+  
+  
+  
