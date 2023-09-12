77f0d23f7f84065fdf5f751cff74bedfa167c5e0
David <davidasousa335@gmail.com>
2023-09-11 22:32:06 -0400

    GitHub API Instead of GraphQL

--- GraphQuery.ts
+++ GraphQuery.ts
@@ -22,36 +22,8 @@ const { owner, repo } = getOwnerAndRepoFromUrl(githubRepoUrl);
 console.log(`Owner: ${owner}`);
 console.log(`Repo: ${repo}`);
 
-
-// Define the GraphQL queries with the owner and repo variables
-const collaboratorQuery = `
-query {
-  repository(owner: "${owner}", name: "${repo}") {
-    collaborators(first: 100) {
-      edges {
-        node {
-          login
-        }
-      }
-    }
-  }
-}
-`;
-
-const commitQuery = (login:any) => `
-query {
-  user(login: "${login}") {
-    contributionsCollection {
-      contributionCalendar {
-        totalContributions
-      }
-    }
-  }
-}
-`;
-
-// GitHub API GraphQL endpoint
-const apiUrl = 'https://api.github.com/graphql';
+// GitHub API REST endpoint for getting contributors
+const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contributors`;
 
 // Set up the request headers with your GitHub Personal Access Token
 const headers = {
@@ -59,38 +31,21 @@ const headers = {
   'Content-Type': 'application/json',
 };
 
-// Checking The Validity Of The API Key
-axios.get(apiUrl, { headers })
-  .then(response => {
-    if (response.status === 200) {
-      console.log('Personal Access Token is valid.');
-    } else {
-      console.error('Personal Access Token is not valid.');
-    }
-  })
-  .catch(error => {
-    console.error('Error:', error.message);
-    if (error.response) {
-      console.error('Response Status Code:', error.response.status);
-    }
-  });
-
 // Function to fetch the total number of commits per user
 const fetchCommitsPerUser = async () => {
   try {
-    const collaboratorResponse = await axios.post(apiUrl, { query: collaboratorQuery }, { headers });
-    const collaborators = collaboratorResponse.data.data.repository.collaborators.edges;
+    const response = await axios.get(apiUrl, { headers });
+    const contributors = response.data;
 
-    if (collaborators) {
+    if (Array.isArray(contributors)) {
       console.log(`Total number of commits per user for ${owner}/${repo}:`);
-      for (const { node } of collaborators) {
-        const login = node.login;
-        const commitResponse = await axios.post(apiUrl, { query: commitQuery(login) }, { headers });
-        const totalCommits = commitResponse.data.data.user.contributionsCollection.contributionCalendar.totalContributions;
+      for (const contributor of contributors) {
+        const login = contributor.login;
+        const totalCommits = contributor.contributions;
         console.log(`${login}: ${totalCommits} commits`);
       }
     } else {
-      console.error('Unable to retrieve the list of collaborators.');
+      console.error('Unable to retrieve the list of contributors.');
     }
   } catch (error) {
     console.error('Error:', error);
