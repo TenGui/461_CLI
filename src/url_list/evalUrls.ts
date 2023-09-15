4ac7e25eda28029ad36a905e38f0036c9d476dfc
TenGui <84549255+TenGui@users.noreply.github.com>
2023-09-15 09:00:20 -0400

    Rampup (#54)
    * initial
    
    * counts lines of MD and JS in repo
    
    * ramp up working, except for big libraries like typescript
    
    * changed rampup equation

--- src/url_list/evalUrls.ts
+++ src/url_list/evalUrls.ts
@@ -1,7 +1,12 @@
 import { RateLimiter } from "../utils/apiRateLimit";
 import { get_urls, parseFromGitLink } from "../utils/utils";
 import * as LicenseRunner from "./licenseMetric/licenseRunner";
+<<<<<<< HEAD
 import * as BusFactorRunner from "./busFactorMetric/busFactorRunner";
+=======
+import { getRampUpScore } from "./rampUpMetric/rampUpRunner";
+
+>>>>>>> 5e4f7a1 (Rampup (#54))
 async function eval_file(filepath: string = "URL_FILE_PATH"): Promise<void> {
   const url_list = get_urls(filepath);
   url_list.forEach(async (urlstr) => {
@@ -11,11 +16,18 @@ async function eval_file(filepath: string = "URL_FILE_PATH"): Promise<void> {
       limiter,
       `/repos/${url[0]}/${url[1]}`
     );
+<<<<<<< HEAD
     const busFactorScore = await BusFactorRunner.getBusFactorScore(
       limiter, 
       `/repos/${url[0]}/${url[1]}`
     );
     console.log(urlstr, licenseScore, busFactorScore);
+=======
+    console.log(urlstr, licenseScore);
+
+    const rampUpScore = await getRampUpScore(url);
+    console.log(urlstr, rampUpScore);
+>>>>>>> 5e4f7a1 (Rampup (#54))
     //do something
   });
 }
