8f6ea706f2427e64b147b827ad34254ae3469c4b
TenGui <cyrus.z.xu@gmail.com>
2023-09-15 08:57:36 -0400

    changed rampup equation

--- src/url_list/rampUpMetric/calcRampUpScore.ts
+++ src/url_list/rampUpMetric/calcRampUpScore.ts
@@ -2,7 +2,7 @@ function calcRampUpScore(linesMD: number, linesJS: number): number {
   if (linesJS <= 0) {
     return 0;
   }
-  const denom = 1 + Math.exp((-3 * linesMD) / linesJS);
+  const denom = 1 + Math.exp((-5 * linesMD) / linesJS);
   const res = 2 / denom - 1;
   return res;
 }
