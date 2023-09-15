be2e23a4e8f44ce60f917fa939de9f2b6a409d8a
ynnos0111 <87532510+ynnos0111@users.noreply.github.com>
2023-09-14 20:51:51 -0400

    Bus Factor and changes in License runner

--- src/url_list/evalUrls.ts
+++ src/url_list/evalUrls.ts
@@ -1,7 +1,7 @@
 import { RateLimiter } from "../utils/apiRateLimit";
 import { get_urls, parseFromGitLink } from "../utils/utils";
 import * as LicenseRunner from "./licenseMetric/licenseRunner";
-
+import * as BusFactorRunner from "./busFactorMetric/busFactorRunner";
 async function eval_file(filepath: string = "URL_FILE_PATH"): Promise<void> {
   const url_list = get_urls(filepath);
   url_list.forEach(async (urlstr) => {
@@ -11,7 +11,11 @@ async function eval_file(filepath: string = "URL_FILE_PATH"): Promise<void> {
       limiter,
       `/repos/${url[0]}/${url[1]}`
     );
-    console.log(urlstr, licenseScore);
+    const busFactorScore = await BusFactorRunner.getBusFactorScore(
+      limiter, 
+      `/repos/${url[0]}/${url[1]}`
+    );
+    console.log(urlstr, licenseScore, busFactorScore);
     //do something
   });
 }
--- src/url_list/licenseMetric/licenseRunner.ts
+++ src/url_list/licenseMetric/licenseRunner.ts
@@ -1,7 +1,26 @@
 async function getLicenseScore(RateLimiter: any, url: string): Promise<number> {
     const License = "LGPL-2.1";
     const _license = await RateLimiter.getGitHubInfo(url + "/license");
-    return _license.license.spdx_id == License ? 1 : 0;
+    if (_license == null) {
+        let license = "";
+        const readme = await RateLimiter.getGitHubInfo(url + "/readme");
+        const content = readme.content;
+        const decodeedContent = Buffer.from(content, "base64").toString();
+        const Licensesection = decodeedContent.split("License")[1];
+        if (Licensesection.length == 1) {
+            return 0;
+        }
+        const licenseRegex = /([^ ]+)\s+license/i;;
+        const match = Licensesection.match(licenseRegex);
+        if (match && match[1]) {
+            license = match[1];
+        }
+        return license == License ? 1 : 0;
+    }
+    else
+        return _license.license.spdx_id == License ? 1 : 0;
+
+    return 1;
 }
 
 export { getLicenseScore };
