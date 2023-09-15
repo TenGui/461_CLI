6e3fce3124c5b19110e7c5364d413e02b816a5eb
TenGui <cyrus.z.xu@gmail.com>
2023-09-15 13:58:00 -0400

    rebase idk

--- src/url_list/evalUrls.ts
+++ src/url_list/evalUrls.ts
@@ -2,9 +2,14 @@ import { RateLimiter } from "../utils/apiRateLimit";
 import { get_urls, parseFromGitLink } from "../utils/utils";
 import * as LicenseRunner from "./licenseMetric/licenseRunner";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import * as BusFactorRunner from "./busFactorMetric/busFactorRunner";
 =======
 import { getRampUpScore } from "./rampUpMetric/rampUpRunner";
+=======
+import * as RampUpRunner from "./rampUpMetric/rampUpRunner";
+import * as BusFactorRunner from "./busFactorMetric/busFactorRunner";
+>>>>>>> 913bfdf (adjusted rampUp call, commented out rampUp, fixed merge conflicts)
 
 >>>>>>> 5e4f7a1 (Rampup (#54))
 async function eval_file(filepath: string = "URL_FILE_PATH"): Promise<void> {
@@ -25,9 +30,21 @@ async function eval_file(filepath: string = "URL_FILE_PATH"): Promise<void> {
 =======
     console.log(urlstr, licenseScore);
 
+<<<<<<< HEAD
     const rampUpScore = await getRampUpScore(url);
     console.log(urlstr, rampUpScore);
 >>>>>>> 5e4f7a1 (Rampup (#54))
+=======
+    // const rampUpScore = await RampUpRunner.getRampUpScore(url);
+    // console.log(urlstr, rampUpScore);
+    // need to fix, running too long
+
+    const busFactorScore = await BusFactorRunner.getBusFactorScore(
+      limiter,
+      `/repos/${url[0]}/${url[1]}`
+    );
+    console.log(urlstr, busFactorScore);
+>>>>>>> 913bfdf (adjusted rampUp call, commented out rampUp, fixed merge conflicts)
     //do something
   });
 }
