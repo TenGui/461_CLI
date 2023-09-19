8c7e48ce87ced8917424c2c90a12a03dafd3ef7a
Shao Ning Huang <huan1645@purdue.edu>
2023-09-19 15:06:25 -0400

    Bf licenseandtest (#70)
    * test for bs license
    
    * test function
    
    * solve conflict
    
    * solve conflict
    
    ---------
    
    Co-authored-by: ynnos0111 <87532510+ynnos0111@users.noreply.github.com>
    Co-authored-by: TenGui <84549255+TenGui@users.noreply.github.com>

--- src/run.ts
+++ src/run.ts
@@ -2,7 +2,7 @@
 import * as runInstall from "./install/installProcess";
 import * as evalUrls from "./url_list/evalUrls";
 import * as dotenv from "dotenv";
-
+import * as child_process from "child_process";
 dotenv.config();
 
 if (process.argv.length != 3) {
@@ -14,8 +14,16 @@ if (process.argv[2] == "install") {
   const installResult: boolean = runInstall.install();
   installResult ? process.exit(0) : process.exit(1);
 } else if (process.argv[2] == "test") {
-  console.log("Test not yet implemented");
-  process.exit(1);
+  try {
+    child_process.execSync("npm test", { stdio: "inherit"});
+    process.exit(0);
+  }
+  catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  
 } else {
   const filePath: string = process.argv[2];
   evalUrls.eval_file(filePath);
