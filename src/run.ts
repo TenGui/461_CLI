988359468d568c57a6a5f87676cd58dfaaef8cc0
ynnos0111 <87532510+ynnos0111@users.noreply.github.com>
2023-09-20 17:25:50 -0400

    Revert "Testing feature"
    This reverts commit 45ecdf1312919cb9bff2ce3da915fc112ea2a65c.

--- src/run.ts
+++ src/run.ts
@@ -3,8 +3,6 @@ import * as runInstall from "./install/installProcess";
 import * as evalUrls from "./url_list/evalUrls";
 import * as dotenv from "dotenv";
 import * as child_process from "child_process";
-import * as fs from "fs";
-import * as path from "path";
 dotenv.config();
 
 if (process.argv.length != 3) {
@@ -17,30 +15,11 @@ if (process.argv[2] == "install") {
   installResult ? process.exit(0) : process.exit(1);
 } else if (process.argv[2] == "test") {
   try {
-    const npmTestProcess = child_process.spawnSync("npm", ["test"], { encoding: "utf8" });
-    const output = npmTestProcess.output;
-    let outputInfo = "";
-    for (const line of output) {
-      if (line && line.includes("Tests:")) {
-        outputInfo = line.trim();
-        break;
-      }
-    }
-    
-    const testCasesPassedRegex = /Tests:\s+(\d+)\s+passed,\s+(\d+)\s+total/;
-    const testCasesPassed = outputInfo.match(testCasesPassedRegex);
-    if (!testCasesPassed) {
-      process.exit(1);
-    }
-    else{
-      const passed: number = parseInt(testCasesPassed[1]);
-      const total: number = parseInt(testCasesPassed[2]);
-      const coverage = JSON.parse(fs.readFileSync(path.join(process.cwd(), "/coverage/coverage-summary.json"), {encoding: "utf8"}));
-      const line_coverage = coverage.total.lines.pct;
-      console.log(`${passed}/${total} test cases passed. ${line_coverage}% line coverage achieved`);
-    }
+    child_process.execSync("npm test", { stdio: "inherit"});
+    process.exit(0);
   }
   catch (err) {
+    console.log(err);
     process.exit(1);
   }
 
--- src/url_list/rampUpMetric/rampUpUtils.ts
+++ src/url_list/rampUpMetric/rampUpUtils.ts
@@ -8,7 +8,7 @@ async function countLines(dir: string): Promise<number> {
 
 function getFileWithEnd(postfix: string, fileList: string[]): string[] {
   const filteredList = fileList.filter((filename: string) => {
-    return filename.endsWith(postfix);
+    return filename.endsWith(".md");
   });
   return filteredList;
 }
