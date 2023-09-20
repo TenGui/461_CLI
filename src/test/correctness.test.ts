f383e43547cf15a98970d6391f9c746e78e1d663
ynnos0111 <87532510+ynnos0111@users.noreply.github.com>
2023-09-20 17:27:28 -0400

    Testing feature

--- src/test/correctness.test.ts
+++ src/test/correctness.test.ts
@@ -1,9 +1,26 @@
 import {calcCorrectnessScore} from "../url_list/correctnessMetric/correctnessRunner";
 
 describe("Correctness Metric", () => {
-    //sohuld return 0.75 instead of 1?
+    //sohuld return 0.75?
     it('Should Return 1', async () => {
         const t1 = await calcCorrectnessScore(1,1,1,1);
         expect(t1).toEqual(0.75);
-    });    
+    });
+    /*
+    //should return 0.5?
+    it('Should Return 0.75', async () => {
+        const t2 = await calcCorrectnessScore(1,1,1,0.5);
+        expect(t2).toEqual(0.5);
+    });
+
+    //shoud return 0.
+    it('Should Return 0.5', async () => {
+        const t3 = await calcCorrectnessScore(1,1,1,0);
+        expect(t3).toEqual(0.5);
+    });
+    it('Should Return 0.5', async () => {
+        const t4 = await calcCorrectnessScore(0.5,1,1,1);
+        expect(t4).toEqual(0.5);
+    });
+    */
 });
