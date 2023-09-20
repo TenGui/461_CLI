45ecdf1312919cb9bff2ce3da915fc112ea2a65c
ynnos0111 <87532510+ynnos0111@users.noreply.github.com>
2023-09-20 17:23:07 -0400

    Testing feature

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,9 @@ module.exports = {
   //   "^.+\\.js$": "babel-jest",
   // },
   // setupFiles: ["<rootDir>/setupTests.js"], // Run setupTests.js before tests.
-  // collectCoverage: true, // Enable code coverage reporting.
-   coverageReporters: ["html", "text-summary"], // Specify coverage report formats.
-  // reporters: ["default"], // Use the default test reporter.
+   collectCoverage: true, // Enable code coverage reporting.
+   coverageReporters: ["lcov", "text-summary", "json-summary"], // Specify coverage report formats.
+   reporters: ["default"], // Use the default test reporter.
   // moduleNameMapper: {
   //   // Map module imports to mock files.
   //   "\\.(css|less|scss)$": "identity-obj-proxy",
