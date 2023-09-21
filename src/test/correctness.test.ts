d14c6b119ad601d83a6325816682bba16c21dc51
ynnos0111 <87532510+ynnos0111@users.noreply.github.com>
2023-09-21 14:39:42 -0400

    Testing function completed, can log when there's test failed

--- src/test/correctness.test.ts
+++ src/test/correctness.test.ts
@@ -6,13 +6,13 @@ describe("Correctness Metric", () => {
         const t1 = await calcCorrectnessScore(1,1,1,1);
         expect(t1).toEqual(0.75);
     });
-    /*
+    
     //should return 0.5?
     it('Should Return 0.75', async () => {
         const t2 = await calcCorrectnessScore(1,1,1,0.5);
         expect(t2).toEqual(0.5);
     });
-
+    
     //shoud return 0.
     it('Should Return 0.5', async () => {
         const t3 = await calcCorrectnessScore(1,1,1,0);
@@ -22,5 +22,4 @@ describe("Correctness Metric", () => {
         const t4 = await calcCorrectnessScore(0.5,1,1,1);
         expect(t4).toEqual(0.5);
     });
-    */
 });
