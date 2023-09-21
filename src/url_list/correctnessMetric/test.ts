e9fdd925140cbebe4f0f62ad40a8d1715d751451
David <davidasousa335@gmail.com>
2023-09-21 14:18:22 -0400

    Added Another Test For Correctness Metric

--- src/url_list/correctnessMetric/test.ts
+++ src/url_list/correctnessMetric/test.ts
@@ -7,9 +7,12 @@ async function main() {
     // https://github.com/MonoGame/MonoGame
     // https://github.com/coqui-ai/TTS
     // https://github.com/meshery/meshery
+    // https://github.com/williamyang1991/Rerender_A_Video
+    // https://github.com/spatie/laravel-event-sourcing
+    //
     // For The Stat To Work The Repo Needs To Have Valid Release Assets
 
-    const correctness = await getCorrectness("https://github.com/williamyang1991/Rerender_A_Video");
+    const correctness = await getCorrectness("https://github.com/spatie/laravel-event-sourcing");
     console.log(correctness);
 }
 
