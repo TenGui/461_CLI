42f7abf28dde4ea5724d303bc769f260d932f348
Chuhan Lin <chuhanlin.business@gmail.com>
2023-10-19 11:03:02 -0400

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,5 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/461_Team4_Phase2/deploy.log
 
-echo 'pm2 restart ECE461-current-web-app' >> /home/ec2-user/461_Team4_Phase2/deploy.log
-pm2 restart ECE461-current-web-app >> /home/ec2-user/461_Team4_Phase2/deploy.log
+echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/461_Team4_Phase2/deploy.log
+pm2 restart nodejs-express-app >> /home/ec2-user/461_Team4_Phase2/deploy.log
