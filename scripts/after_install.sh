29b2cd4d4bcaad79503c63b0cd5f4b4c18268372
Chuhan Lin <chuhanlin.business@gmail.com>
2023-10-26 15:38:07 -0400

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo 'run after_install.sh: ' >> /home/ec2-user/461_Team4_Phase2/deploy.log
 
-echo 'cd /home/ec2-user/461_Team4_Phase2' >> /home/ec2-user/461_Team4_Phase2/deploy.log
-cd /home/ec2-user/461_Team4_Phase2 >> /home/ec2-user/461_Team4_Phase2/deploy.log
+echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/461_Team4_Phase2/deploy.log
+cd /home/ec2-user/nodejs-aws-codedeploy-pipeline >> /home/ec2-user/461_Team4_Phase2/deploy.log
 
 echo 'npm install' >> /home/ec2-user/461_Team4_Phase2/deploy.log 
 npm install >> /home/ec2-user/461_Team4_Phase2/deploy.log
