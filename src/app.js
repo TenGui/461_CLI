e3a9e17e229bb116a0d16910f98601283af14bb3
Chuhan Lin <chuhanlin.business@gmail.com>
2023-11-11 16:00:42 -0500

    Fixed node.js app errors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.pool = exports.promisePool = void 0;
 var path = require("path");
 var http = require("http");
-var mysql = require("mysql2");
 var oas3Tools = require("oas3-tools");
 var authorization_1 = require("./database_files/authorization");
-var serverPort = 8080;
+var serverPort = 3000;
 // swaggerRouter configuration
 var options = {
     routing: {
@@ -30,16 +28,6 @@ var options = {
     // swaggerUIts: {},
     // swaggerUI: {}
 };
-var pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'mydb',
-    connectionLimit: 5, // Adjust as needed
-});
-exports.pool = pool;
-var promisePool = pool.promise();
-exports.promisePool = promisePool;
 var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
 var app = expressAppConfig.getApp();
 // Initialize the Swagger middleware
