64aab74e74dc3f941f163e3e7535fc7be3777d22
Chuhan Lin <chuhanlin.business@gmail.com>
2023-11-11 16:45:52 -0500

    Changed database configs

--- src/database_files/database_connect.ts
+++ src/database_files/database_connect.ts
@@ -2,11 +2,11 @@ import * as mysql from "mysql2";
 require('dotenv').config();
 
 const db = mysql.createPool({
-    host: "publicdb.cvr1hbjvaden.us-east-2.rds.amazonaws.com",
+    host: process.env.DB_HOST,
     port: 3306,
-    user: "admin",
-    password: "project461",
-    database: "testdb",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     connectionLimit: 5, // Adjust as needed
 });
 const promisePool = db.promise();
