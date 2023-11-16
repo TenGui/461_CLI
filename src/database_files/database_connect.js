a4e742080cefb7c5f4dab998538279145c2b96ff
Chuhan Lin <chuhanlin.business@gmail.com>
2023-11-15 22:48:36 -0500

    Finished reset-endpoint

--- src/database_files/database_connect.js
+++ src/database_files/database_connect.js
@@ -3,28 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = exports.promisePool = void 0;
 var mysql = require("mysql2");
 require('dotenv').config();
-var db = mysql.createPool({
-    host: "publicdb.cvr1hbjvaden.us-east-2.rds.amazonaws.com",
+var db = mysql.createConnection({
+    host: process.env.DB_HOST,
     port: 3306,
-    user: "admin",
-    password: "project461",
-    database: "testdb",
-    connectionLimit: 5, // Adjust as needed
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
 exports.db = db;
 var promisePool = db.promise();
 exports.promisePool = promisePool;
-// const db = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     port: 3306,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABASE,
-// });
-// db.connect((err) => {
-//     if (err) {
-//         console.log(err);
-//         return;
-//     }
-//     console.log("DB connected");
-// });
