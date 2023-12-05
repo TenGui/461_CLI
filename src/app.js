d5498100bb4879ab60a002de0038749c4b17b152
JDCambridge <56004959+JDCambridge@users.noreply.github.com>
2023-12-05 18:32:03 -0500

    logger writing to file correctly and no longer dumping whole zip files

--- src/app.js
+++ src/app.js
@@ -24,20 +24,42 @@ morgan.token('auth', function (req, res) {
 });
 morgan.token('resBody', function (req, res) {
     ;
-    return JSON.stringify(res.body);
+    //make new json to return
+    var tempBody = JSON.parse(JSON.stringify(res.body));
+    if (res.body.hasOwnProperty("Content")) {
+        tempBody["Content"] = tempBody["Content"].length;
+    }
+    if (res.body.hasOwnProperty("PackageData")) {
+        if (res.hasOwnProperty("PackageData")) {
+            tempBody["PackageData"]["Content"] = tempBody["PackageData"]["Content"].length;
+        }
+    }
+    return JSON.stringify(tempBody);
 });
 morgan.token('reqBody', function (req, res) {
     ;
-    return JSON.stringify(req.body);
+    //make new json to return
+    var tempBody = JSON.parse(JSON.stringify(req.body));
+    if (req.body.hasOwnProperty("Content")) {
+        tempBody["Content"] = tempBody["Content"].length;
+    }
+    if (req.body.hasOwnProperty("PackageData")) {
+        if (req.hasOwnProperty("PackageData")) {
+            tempBody["PackageData"]["Content"] = tempBody["PackageData"]["Content"].length;
+        }
+    }
+    return JSON.stringify(tempBody);
 });
 morgan.token('uuid', function (req, res) {
     ;
     return req.requestId;
 });
-var reqFormat = "reqID\::uuid :remote-addr - :remote-user [:date[clf]] \":method :url HTTP/:http-version\" authHeader\: :auth reqBody\: :reqBody";
-var resFormat = "reqID\::uuid :remote-addr - :remote-user [:date[clf]] \":method :url HTTP/:http-version\" :status  authHeader\: :auth resBody\: :resBody";
+//make format strings for logging 
+var reqFormat = "REQUEST reqID\::uuid :remote-addr - :remote-user [:date[clf]] \":method :url HTTP/:http-version\" authHeader\: :auth reqBody\: :reqBody";
+var resFormat = "RESPONSE reqID\::uuid :remote-addr - :remote-user [:date[clf]] \":method :url HTTP/:http-version\" :status  authHeader\: :auth resBody\: :resBody";
 // create a write stream (in append mode) for the logger
-var immediateLogStream = fs.createWriteStream(path.join(__dirname, 'req.log'), { flags: 'a' });
+var logStream = fs.createWriteStream(path.join(__dirname, 'req.log'), { flags: 'a' });
+;
 var serverPort = 3000;
 // swaggerRouter configuration
 var options = {
@@ -55,30 +77,28 @@ var options = {
         validateSecurity: true,
         validateFormats: 'full',
     },
-    logging: {
-        format: reqFormat,
-        options: { stream: immediateLogStream,
-            immediate: true },
-        level: 'info',
-    },
+    // logging: {
+    //     format: 'commmon',
+    //     level: 'info',
+    // },
     // swaggerUIts: {},
     // swaggerUI: {}
 };
-// create a write stream (in append mode)
-var exitLogStream = fs.createWriteStream(path.join(__dirname, 'res.log'), { flags: 'a' });
-var newFormat = 'combined';
-var newOptions = { stream: exitLogStream };
-var resLogger = morgan(resFormat, newOptions);
-// function newMiddleWare(req, res, next) {
-//     console.log('Time:', Date.now())
-//     next()
-// }
 var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
 var app = expressAppConfig.getApp();
+//create a write stream (in append mode)
+var logOptions = { stream: logStream };
+var immediateOptions = {
+    stream: logStream,
+    immediate: true
+};
+var resLogger = morgan(resFormat, logOptions);
 app.use(resLogger);
+var reqLogger = morgan(reqFormat, immediateOptions);
+app.use(reqLogger);
 //hack solution to dump a logger in at the end of the middleware stack
 var stack = app._router.stack;
-var lastEntries = stack.splice(app._router.stack.length - 1); // The number of middle ware added
+var lastEntries = stack.splice(app._router.stack.length - 2); // The number of middle ware added
 var firstEntries = stack.splice(0, 15); //How many middlewares should come before the new one
 app._router.stack = __spreadArray(__spreadArray(__spreadArray([], firstEntries, true), lastEntries, true), stack, true);
 //console.log(app._router.stack);
@@ -94,7 +114,7 @@ var stack2 = app._router.stack;
 var lastEntries2 = stack2.splice(app._router.stack.length - 1); // The number of middle ware added
 var firstEntries2 = stack2.splice(0, 1); //How many middlewares should come before the new one
 app._router.stack = __spreadArray(__spreadArray(__spreadArray([], firstEntries2, true), lastEntries2, true), stack2, true);
-console.log(app._router.stack);
+//console.log(app._router.stack);
 // // Initialize the Swagger middleware
 http.createServer(app).listen(serverPort, function () {
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
--- src/app.ts
+++ src/app.ts
@@ -15,16 +15,36 @@ const uuid = require('uuid');
 morgan.token('auth', function (req, res) { ;
     return req.header('X-Authorization')});
 morgan.token('resBody', function (req, res) { ;
-    return JSON.stringify(res.body)});
+    //make new json to return
+    let tempBody:JSON = JSON.parse(JSON.stringify(res.body));
+    if(res.body.hasOwnProperty("Content")){
+        tempBody["Content"] = tempBody["Content"].length;
+    }
+    if(res.body.hasOwnProperty("PackageData")){
+        if(res.hasOwnProperty("PackageData")){
+            tempBody["PackageData"]["Content"] = tempBody["PackageData"]["Content"].length;
+        }
+    }
+    return JSON.stringify(tempBody)});  
 morgan.token('reqBody', function (req, res) { ;
-    return JSON.stringify(req.body)});    
+    //make new json to return
+    let tempBody:JSON = JSON.parse(JSON.stringify(req.body));
+    if(req.body.hasOwnProperty("Content")){
+        tempBody["Content"] = tempBody["Content"].length;
+    }
+    if(req.body.hasOwnProperty("PackageData")){
+        if(req.hasOwnProperty("PackageData")){
+            tempBody["PackageData"]["Content"] = tempBody["PackageData"]["Content"].length;
+        }
+    }
+    return JSON.stringify(tempBody)});    
 morgan.token('uuid', function (req, res) { ;
-    return req.requestId});        
+    return req.requestId});
 //make format strings for logging 
-const reqFormat = "reqID\::uuid :remote-addr - :remote-user [:date[clf]] \":method :url HTTP/:http-version\" authHeader\: :auth reqBody\: :reqBody";
-const resFormat = "reqID\::uuid :remote-addr - :remote-user [:date[clf]] \":method :url HTTP/:http-version\" :status  authHeader\: :auth resBody\: :resBody";
+const reqFormat = "REQUEST reqID\::uuid :remote-addr - :remote-user [:date[clf]] \":method :url HTTP/:http-version\" authHeader\: :auth reqBody\: :reqBody";
+const resFormat = "RESPONSE reqID\::uuid :remote-addr - :remote-user [:date[clf]] \":method :url HTTP/:http-version\" :status  authHeader\: :auth resBody\: :resBody";
 // create a write stream (in append mode) for the logger
-var immediateLogStream = fs.createWriteStream(path.join(__dirname, 'req.log'), { flags: 'a' });
+var logStream = fs.createWriteStream(path.join(__dirname, 'req.log'), { flags: 'a' });
 //setup typedef for uuid field in request so that typescript doesn't throw a fit 
 declare global {
     namespace Express {
@@ -52,12 +72,10 @@ const options: any =  {
         validateSecurity: true,
         validateFormats: 'full',
     },
-    logging: {
-        format: reqFormat,
-        options: { stream: immediateLogStream,
-        immediate: true },
-        level: 'info',
-    },
+    // logging: {
+    //     format: 'commmon',
+    //     level: 'info',
+    // },
     // swaggerUIts: {},
     // swaggerUI: {}
 };
@@ -68,16 +86,22 @@ const app = expressAppConfig.getApp();
 
 
 
-// create a write stream (in append mode)
-var resLogStream = fs.createWriteStream(path.join(__dirname, 'res.log'), { flags: 'a' });
-let resOptions = {stream: resLogStream};
-const resLogger = morgan(resFormat, resOptions);
+//create a write stream (in append mode)
+let logOptions = {stream: logStream};
+let immediateOptions = {
+    stream: logStream,
+    immediate: true
+};
+const resLogger = morgan(resFormat, logOptions);
 app.use(resLogger);
+
+const reqLogger = morgan(reqFormat, immediateOptions);
+app.use(reqLogger);
 
 
 //hack solution to dump a logger in at the end of the middleware stack
 const stack = app._router.stack;
-const lastEntries = stack.splice(app._router.stack.length - 1);  // The number of middle ware added
+const lastEntries = stack.splice(app._router.stack.length - 2);  // The number of middle ware added
 const firstEntries = stack.splice(0, 15); //How many middlewares should come before the new one
 app._router.stack = [...firstEntries, ...lastEntries, ...stack];
 //console.log(app._router.stack);
@@ -94,7 +118,7 @@ const stack2 = app._router.stack;
 const lastEntries2 = stack2.splice(app._router.stack.length - 1);  // The number of middle ware added
 const firstEntries2 = stack2.splice(0, 1); //How many middlewares should come before the new one
 app._router.stack = [...firstEntries2, ...lastEntries2, ...stack2];
-console.log(app._router.stack);
+//console.log(app._router.stack);
 
 
 
