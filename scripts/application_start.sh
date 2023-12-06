691ea7212e13eb6741ba55299d1d4cbcaaff4cdb
Chuhan Lin <chuhanlin.business@gmail.com>
2023-12-05 19:03:18 -0500

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,3 +5,4 @@ cd /home/ec2-user/461_Team4_Phase2 >> /home/ec2-user/461_Team4_Phase2/deploy.log
 
 echo 'pm2 restart 461_Team4_Phase2 --update-env' >> /home/ec2-user/461_Team4_Phase2/deploy.log
 pm2 restart 461_Team4_Phase2 --update-env >> /home/ec2-user/461_Team4_Phase2/deploy.log
+pm2 restart app --update-env >> /home/ec2-user/461_Team4_Phase2/deploy.log
